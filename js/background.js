537ce020d20c527a3b1151f064caa9b73ad801a9
jx3-luoying <jx3-luoying@users.noreply.github.com>
2014-04-11 16:53:51 -0700

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,13 +1,13 @@
 var currentTime = new Date().getHours();
-document.body.background-repeat=no-repeat;
-document.body.background-position=center;
 if (7 <= currentTime && currentTime < 20) {
     if (document.body) {
-        document.body.background = "http://thebus.net/sites/default/files/ws_Good_Morning_Sun_1280x1024.jpg";
+        document.body.background-repeat=no-repeat;
+        document.body.background-position=center;
+        document.body.background-image = "http://thebus.net/sites/default/files/ws_Good_Morning_Sun_1280x1024.jpg";
     }
 }
 else {
     if (document.body) {
-        document.body.background = "http://miriadna.com/desctopwalls/images/max/Last-evening-hour.jpg";
+        document.body.background-image = "http://miriadna.com/desctopwalls/images/max/Last-evening-hour.jpg";
     }
 }
