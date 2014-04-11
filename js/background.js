af87731d81b6c2ce87c6e0a28d70c9c8781de7c6
jx3-luoying <jx3-luoying@users.noreply.github.com>
2014-04-11 16:41:16 -0700

    add background for 2048

---
+++ js/background.js
@@ -0,0 +1,11 @@
+var currentTime = new Date().getHours();
+if (7 <= currentTime && currentTime < 20) {
+    if (document.body) {
+        document.body.background = "http://thebus.net/sites/default/files/ws_Good_Morning_Sun_1280x1024.jpg";
+    }
+}
+else {
+    if (document.body) {
+        document.body.background = "http://miriadna.com/desctopwalls/images/max/Last-evening-hour.jpg";
+    }
+}
