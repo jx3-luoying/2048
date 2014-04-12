a40a5629b86eebb9ccfcf098a925fa2a42257aac
Xinrui (Liz) <xinrui.yu@cogi.com>
2014-04-12 03:57:01 -0700

    add random background

--- js/background.js
+++ js/background.js
@@ -1,58 +1,50 @@
-var num = Math.random()*9;
+var num =  Math.floor(Math.random()*9);
 switch(num){
     case 0:
         if (document.body) {
-            document.body.background-image = url('1.png');
-            document.body.backgournd-color = #88171C;
+            document.body.style.background ="url('style/1.png') #88211C no-repeat left bottom";
         }
         break;
     case 1:
         if (document.body) {
-            document.body.background-image = url('2.png');
-            document.body.backgournd-color = #3D2D2E;
+            document.body.style.background ="url('style/2.png') #3D2D2E no-repeat right bottom";
         }
         break;
     case 2:
         if (document.body) {
-            document.body.background-image = url('3.png');
-            document.body.backgournd-color = #524439;
+            document.body.style.background ="url('style/3.png') #524439 no-repeat right bottom";
         }
         break;       
     case 3:
         if (document.body) {
-            document.body.background-image = url('4.png');
-            document.body.backgournd-color = #626262;
+            document.body.style.background ="url('style/4.png') #626262 no-repeat left bottom";
         }
         break;
     case 4:
         if (document.body) {
-            document.body.background-image = url('5.png');
-            document.body.backgournd-color = #C76574;
+            document.body.style.background ="url('style/5.png') #C76574 no-repeat right bottom";
         }
         break;
     case 5:
         if (document.body) {
-            document.body.background-image = url('6.png');
-            document.body.backgournd-color = #825D6F;
+            document.body.style.background ="url('style/6.png') #825D6F no-repeat right bottom";
         }
         break;
     case 6:
         if (document.body) {
-            document.body.background-image = url('7.png');
-            document.body.backgournd-color = #6D5C7E;
+            document.body.style.background ="url('style/7.png') #6D5C7E no-repeat right bottom";
         }
         break;
     case 7:
         if (document.body) {
-            document.body.background-image = url('8.png');
-            document.body.backgournd-color = #375270;
+            document.body.style.background ="url('style/8.png') #375270 no-repeat right bottom";
         }
         break;
     case 8:
         if (document.body) {
-            document.body.background-image = url('9.png');
-            document.body.backgournd-color = #235067;
+            document.body.style.background ="url('style/9.png') #235067 no-repeat right bottom";
         }
         break;  
 }
     
+
