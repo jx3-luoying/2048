8090592a15b1a544d71975260cb08c9888ac060b
jx3-luoying <jx3-luoying@users.noreply.github.com>
2014-04-11 19:46:04 -0700

    change background image

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
 var currentTime = new Date().getHours();
 if (7 <= currentTime && currentTime < 20) {
     if (document.body) {
-        document.body.background-image = "http://thebus.net/sites/default/files/ws_Good_Morning_Sun_1280x1024.jpg";
+        document.body.background-image = "http://pic4.178.com/1054/10542337/month_1301/b31a100862232.jpg";//"http://thebus.net/sites/default/files/ws_Good_Morning_Sun_1280x1024.jpg";
     }
 }
 else {
