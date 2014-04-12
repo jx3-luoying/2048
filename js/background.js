4aa21f428f1f1c2dde51273129f8f9fcc0b21c30
jx3-luoying <jx3-luoying@users.noreply.github.com>
2014-04-11 17:04:58 -0700

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,9 +1,7 @@
 var currentTime = new Date().getHours();
 if (7 <= currentTime && currentTime < 20) {
     if (document.body) {
-        document.body.background-repeat=no-repeat;
-        document.body.background-position=center;
-        document.body.background-image = url('http://thebus.net/sites/default/files/ws_Good_Morning_Sun_1280x1024.jpg');
+        document.body.background-image = "http://thebus.net/sites/default/files/ws_Good_Morning_Sun_1280x1024.jpg";
     }
 }
 else {
