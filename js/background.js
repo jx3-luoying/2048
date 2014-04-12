4f5573fb6815e032fcffb8e6b96924b5625874d5
jx3-luoying <jx3-luoying@users.noreply.github.com>
2014-04-12 02:25:52 -0700

    add random background

--- js/background.js
+++ js/background.js
@@ -1,11 +1,58 @@
-var currentTime = new Date().getHours();
-if (7 <= currentTime && currentTime < 20) {
-    if (document.body) {
-        document.body.background-image = "http://thebus.net/sites/default/files/ws_Good_Morning_Sun_1280x1024.jpg";//"http://pic4.178.com/1054/10542337/month_1301/b31a100862232.jpg";//
-    }
+var num = Math.random()*9;
+switch(num){
+    case 0:
+        if (document.body) {
+            document.body.background-image = url('1.png');
+            document.body.backgournd-color = #88171C;
+        }
+        break;
+    case 1:
+        if (document.body) {
+            document.body.background-image = url('2.png');
+            document.body.backgournd-color = #3D2D2E;
+        }
+        break;
+    case 2:
+        if (document.body) {
+            document.body.background-image = url('3.png');
+            document.body.backgournd-color = #524439;
+        }
+        break;       
+    case 3:
+        if (document.body) {
+            document.body.background-image = url('4.png');
+            document.body.backgournd-color = #626262;
+        }
+        break;
+    case 4:
+        if (document.body) {
+            document.body.background-image = url('5.png');
+            document.body.backgournd-color = #C76574;
+        }
+        break;
+    case 5:
+        if (document.body) {
+            document.body.background-image = url('6.png');
+            document.body.backgournd-color = #825D6F;
+        }
+        break;
+    case 6:
+        if (document.body) {
+            document.body.background-image = url('7.png');
+            document.body.backgournd-color = #6D5C7E;
+        }
+        break;
+    case 7:
+        if (document.body) {
+            document.body.background-image = url('8.png');
+            document.body.backgournd-color = #375270;
+        }
+        break;
+    case 8:
+        if (document.body) {
+            document.body.background-image = url('9.png');
+            document.body.backgournd-color = #235067;
+        }
+        break;  
 }
-else {
-    if (document.body) {
-        document.body.background-image = "http://miriadna.com/desctopwalls/images/max/Last-evening-hour.jpg";
-    }
-}
+    
