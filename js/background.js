720eb5983160311eb2e22ccbf67acf6e429ff89d
jx3-luoying <jx3-luoying@users.noreply.github.com>
2014-04-16 01:05:44 -0700

    Update background.js

--- js/background.js
+++ js/background.js
@@ -2,47 +2,47 @@ var num =  Math.floor(Math.random()*9);
 switch(num){
     case 0:
         if (document.body) {
-            document.body.style.background ="url('style/1.jpg') ";//#88211C no-repeat left bottom";
+            document.body.style.background ="url('style/1.jpg') no-repeat left bottom";//#88211C no-repeat left bottom";
         }
         break;
     case 1:
         if (document.body) {
-            document.body.style.background ="url('style/2.jpg') ";//#3D2D2E no-repeat right bottom";
+            document.body.style.background ="url('style/2.jpg') no-repeat right bottom";//#3D2D2E no-repeat right bottom";
         }
         break;
     case 2:
         if (document.body) {
-            document.body.style.background ="url('style/3.jpg') ";//#524439 no-repeat right bottom";
+            document.body.style.background ="url('style/3.jpg') no-repeat right bottom";//#524439 no-repeat right bottom";
         }
         break;       
     case 3:
         if (document.body) {
-            document.body.style.background ="url('style/4.jpg') ";//#626262 no-repeat left bottom";
+            document.body.style.background ="url('style/4.jpg') no-repeat right bottom";//#626262 no-repeat left bottom";
         }
         break;
     case 4:
         if (document.body) {
-            document.body.style.background ="url('style/5.jpg') ";//#C76574 no-repeat right bottom";
+            document.body.style.background ="url('style/5.jpg') no-repeat right bottom";//#C76574 no-repeat right bottom";
         }
         break;
     case 5:
         if (document.body) {
-            document.body.style.background ="url('style/6.jpg') ";//#825D6F no-repeat right bottom";
+            document.body.style.background ="url('style/6.jpg') no-repeat right bottom";//#825D6F no-repeat right bottom";
         }
         break;
     case 6:
         if (document.body) {
-            document.body.style.background ="url('style/7.jpg') ";//#6D5C7E no-repeat right bottom";
+            document.body.style.background ="url('style/7.jpg') no-repeat right bottom";//#6D5C7E no-repeat right bottom";
         }
         break;
     case 7:
         if (document.body) {
-            document.body.style.background ="url('style/8.jpg') ";//#375270 no-repeat right bottom";
+            document.body.style.background ="url('style/8.jpg') no-repeat right bottom";//#375270 no-repeat right bottom";
         }
         break;
     case 8:
         if (document.body) {
-            document.body.style.background ="url('style/9.jpg') ";//#235067 no-repeat right bottom";
+            document.body.style.background ="url('style/9.jpg') no-repeat right bottom";//#235067 no-repeat right bottom";
         }
         break;  
 }
