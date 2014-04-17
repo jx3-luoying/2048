783512ae69a8089eda49d1fc093640753b986a3c
jx3-luoying <jx3-luoying@users.noreply.github.com>
2014-04-17 13:51:03 -0700

    Update level_setting.js

--- js/level_setting.js
+++ js/level_setting.js
@@ -66,13 +66,10 @@ $(function() {
           bValid = bValid && checkLength( $( this ), "tile name", 0, 5 );
         });
         
-        var newList = "<p>";
         if ( bValid ) {
           $( ".input" ).each(function(i) {
             text[i+1]=$( this ).val();
-            //newList = newList + $( this ).val() + " ";
           });
-          $( "#users" ).append( newList + "</p>" );
           $( this ).dialog( "close" );
           $("#levelSetting").slideUp("slow");
         }
